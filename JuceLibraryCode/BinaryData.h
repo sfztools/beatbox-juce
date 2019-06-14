7d92098da6457b99f8fdcfc23ed6e88a19948008
redtide <redtid3@gmail.com>
2019-06-14 22:32:30 +0200

    Fixed header search path.

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -9,7 +9,7 @@
 namespace BinaryData
 {
     extern const char*   LICENSE;
-    const int            LICENSESize = 1174;
+    const int            LICENSESize = 1152;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
     const int namedResourceListSize = 1;
