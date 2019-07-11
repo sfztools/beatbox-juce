7aaaf7f45bf0262b195ef85356d0c4c6632317fd
redtide <redtid3@gmail.com>
2019-07-11 19:28:10 +0200

    Use non-lowercase filenames.

--- Builds/make-appimg.sh
+++ Builds/make-appimg.sh
@@ -4,7 +4,7 @@ app_name="BeatBox"
 build_dir="./LinuxMakefile/build"
 resource_dir="./../resources"
 
-rm -rf "./AppImg"
+rm -rf "./${app_name}"
 
 if [ ! -f "linuxdeploy-x86_64.AppImage" ]; then
 	echo "Downloading Linuxdeploy..."
@@ -14,7 +14,7 @@ else
 	echo "Linuxdeploy already exists, skipping download."
 fi
 for size in 16 32 48 128 256; do
-	dirname="AppImg/usr/share/icons/hicolor/${size}x${size}/apps"
+	dirname="${app_name}/usr/share/icons/hicolor/${size}x${size}/apps"
 	mkdir -p $dirname
 	cp "${resource_dir}/icons/icon_${size}px.png" "./${dirname}/${app_name}.png"
 done
@@ -23,13 +23,13 @@ done
 #       uncomment this when https://github.com/AppImage/AppImageKit/issues/856
 #       and/or 31 will be fixed
 #
-# mkdir -p "./AppImg/usr/share/metainfo/"
+# mkdir -p "./${app_name}/usr/share/metainfo/"
 # cp "${resource_dir}/linux/${app_name}.appdata.xml" "./${app_name}/usr/share/metainfo/"
 
 if [ ! -f "${build_dir}/${app_name}" ]; then
 	./build.sh -r
 fi
-./linuxdeploy-x86_64.AppImage --appdir=AppImg \
+./linuxdeploy-x86_64.AppImage --appdir=${app_name} \
                               --desktop-file=${resource_dir}/linux/${app_name}.desktop \
                               --executable=${build_dir}/${app_name} \
                               --output=appimage
